46a5aec1b37e8f28afe88f3c1fec3ffaad651681
Eve-wambui <shantellemaylyne.em@gmail.com>
2019-11-26 08:55:20 +0300

    round number 1/3

--- 4-standard-lib/numbers.js
+++ 4-standard-lib/numbers.js
@@ -35,7 +35,9 @@ describe("numbers", function() {
             ////////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
             // You will also need to do an additional check here.
-
+                function isNotANumber(number){
+                    return Number.isNaN(number);
+                }
             ////////////////////////////////////////////////////////////////////////
 
             expect(isNotANumber(0)).to.equal(false);
@@ -55,7 +57,9 @@ describe("numbers", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
-
+                function roundNumber(number){
+                    return Math.round(number);
+                }
             ////////////////////////////////////////////////////////////////////////
 
             expect(roundNumber(0)).to.equal(0);
