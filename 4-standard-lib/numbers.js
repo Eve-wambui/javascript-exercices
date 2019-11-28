3d19d822a5425ae986a16b8060f2dd76d82d0925
Eve-wambui <shantellemaylyne.em@gmail.com>
2019-11-28 19:18:22 +0300

    is not a number

--- 4-standard-lib/numbers.js
+++ 4-standard-lib/numbers.js
@@ -36,7 +36,7 @@ describe("numbers", function() {
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
             // You will also need to do an additional check here.
                 function isNotANumber(number){
-                    return Number.isNaN(number);
+                    return typeof number !== "number" || Number.isNaN(number);
                 }
             ////////////////////////////////////////////////////////////////////////
 
